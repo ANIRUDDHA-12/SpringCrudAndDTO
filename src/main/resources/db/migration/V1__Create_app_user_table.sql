66a2a5bcb9f8c3a25e3abe2bdaab02d03a1c6165
Aniruddha-12 <payas.sanjay@gmail.com>
2026-07-13 22:48:52 +0530

    tried flyway for versioning of sql scripts

---
+++ src/main/resources/db/migration/V1__Create_app_user_table.sql
@@ -0,0 +1,6 @@
+  CREATE TABLE app_user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
